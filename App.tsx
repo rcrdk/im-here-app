59d2d286b6a945f1d32c3fcaf6f4482abd8f33ce
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-11-21 08:44:38 -0300

    feat: basics

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.title}>Lorem ipsum dolor sit amet consectetur</Text>
+        <Text style={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet
+          fuga rem eius vel debitis quo velit ipsam numquam illum quos corporis
+          sit neque quia.
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 16,
+    backgroundColor: "#131016",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  box: {
+    padding: 16,
+  },
+  title: {
+    color: "#ffffff",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  text: {
+    marginTop: 12,
+    color: "#ffffff",
+    fontSize: 16,
+    textAlign: "center",
   },
 });
